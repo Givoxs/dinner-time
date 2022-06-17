a028a002af908a8bb0ecde7b62068147fdfafbe0
Givoxs <robert.schipperges@outlook.com>
2022-06-17 14:36:45 +0200

    final

---
+++ src/components/MainProcessButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function MainProcessButton({textbutton}, {classname}){
+
+    <button type="button" className={classname}>
+        text="{textbutton}"
+    </button>
+
+}
+
+export default MainProcessButton
---
+++ src/pages/Footer.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+
+function Feedback() {
+    const [thanks, setThanks] = useState(false);
+    return (
+        <>
+        <footer>
+            <div className="form-container">
+                <h2>Contactform</h2>
+                <form>
+                    <input
+                        type="text"
+                        placeholder="Emailaddress"
+                        name="email"
+                    />
+                    <input
+                        type="text"
+                        placeholder="What do you want to share?"
+                        name="message"
+                    />
+                </form>
+
+            <button className="feedback-button" type="button" onClick={() => setThanks(true)}>
+                Send Message
+            </button>
+
+            {thanks === true && <p>Thanks for your feedback!</p>}
+
+
+        </div>
+        </footer>
+</>
+)
+    ;
+}
+
+export default Feedback;
